0edff34883c7f319c72d8cf9719e8a1e035b8d00
inotom <wdf7322@yahoo.co.jp>
2013-11-08 11:37:27 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Proton::Application.config.secret_key_base = 'f5002a06745d3085dbaf2ca7164d44887064f8aba6c06b38df3c903cbe1c3002c6f02ca64639b2f85141660a843e56a71315fb48e9b8613efa294ab6ad4d8536'

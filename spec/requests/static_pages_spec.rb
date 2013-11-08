6c4891b8a5d82842358ad2b62eb5a2f36a3181ee
inotom <wdf7322@yahoo.co.jp>
2013-11-08 21:02:49 +0900

    Update routings

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,44 +6,44 @@ describe "Static Pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Proton'" do
-      visit '/static_pages/home'
+    it "should have the h1 'Proton'" do
+      visit root_path
       expect(page).to have_content('Proton')
     end
 
-    it "should have the right title" do
-      visit '/static_pages/home'
+    it "should have the base title" do
+      visit root_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('HOME - ')
     end
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
+    it "should have the h1 'Help'" do
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Help - #{base_title}")
     end
   end
 
   describe "About page" do
 
-    it "should have the content 'About Proton'" do
-      visit '/static_pages/about'
+    it "should have the h1 'About Proton'" do
+      visit about_path
       expect(page).to have_content('About Proton')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("About - #{base_title}")
     end
   end

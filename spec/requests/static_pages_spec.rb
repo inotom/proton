ee46a5419ac14ee94b85fbd57a37a11702508ac3
inotom <wdf7322@yahoo.co.jp>
2013-11-08 16:13:07 +0900

    Not customize page tilte in home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,7 +13,12 @@ describe "Static Pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Home - #{base_title}")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('HOME - ')
     end
   end
 

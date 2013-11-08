6e8f425f500d04387c8e0a151244ad1704e4c974
inotom <wdf7322@yahoo.co.jp>
2013-11-08 22:20:16 +0900

    Add user sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Proton::Application.routes.draw do
-  #get 'static_pages3home'
-  #get 'static_pages/help'
-  #get 'static_pages/about'
+  get 'users/new'
 
   root 'static_pages#home'
-  match '/help', to: 'static_pages#help'  , via: 'get'
-  match '/about', to: 'static_pages#about', via: 'get'
+  match '/signup', to: 'users#new'         , via: 'get'
+  match '/help'  , to: 'static_pages#help' , via: 'get'
+  match '/about' , to: 'static_pages#about', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
